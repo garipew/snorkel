5cd7f8e7352f51d511301bfad3c5fd9e95a3087c
Gabriel Sá <gabriel10043@live.com>
2025-10-23 21:38:34 -0300

    modify arena_grow: first allocation defines arena personality

--- arena.c
+++ arena.c
@@ -1,6 +1,7 @@
 #include "arena.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <sys/mman.h>
 
 #define round_align(start, align) \
@@ -10,35 +11,34 @@
 	(round_align((uintptr_t)region->avail, align) + size < (uintptr_t)region->limit)
 
 
-void arena_grow(Arena *arena){
-	void *new_block = mmap(NULL, REGION_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+void* arena_grow(Arena *arena, size_t at_least){
+	if(arena->region_size < REGION_SIZE){
+		arena->region_size = REGION_SIZE;
+	}
+	if(at_least > REGION_SIZE && !arena->start){
+		arena->region_size = round_align(sizeof(Region)+at_least, 16);
+	}
+	void *new_block = mmap(NULL, arena->region_size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
 	if(new_block == MAP_FAILED){
-		return;
+		return NULL;
 	}
 	Region *new_region = new_block;
-	new_region->avail = (unsigned char*)new_region + sizeof(*new_region);
-	new_region->limit = (unsigned char*)new_region + REGION_SIZE;
+	new_region->avail = (u8*)new_region + sizeof(*new_region);
+	new_region->limit = (u8*)new_region + arena->region_size;
 	arena->current = new_region;
 	if(!arena->start){
 		arena->start = new_region;
 		arena->end = arena->start;
-		return;
+		return arena->current;
 	}
 	arena->end->next = new_region;
 	arena->end = new_region;
+	return arena->current;
 }
 
 int find_space(Arena *arena, size_t size, size_t align){
 	for(; arena->current; arena->current = arena->current->next){
 		if(have_space(arena->current, size, align)){
-			// TODO(garipew): Unsure where to clean the memory, right now it cleans
-			// only when it is about to be used. But here sometimes it might clean
-			// already cleaned memory. The other option is to do it in arena_reset,
-			// but then it could get very heavy. I could also use a flag "dirty" and
-			// just clean memory that has it set, but then we are starting to get very
-			// verbose, I don't like the idea very much.
-			// TLDR: Find a better place for cleanup
-			memset(arena->current->avail, 0, arena->current->limit-arena->current->avail);
 			return 1;
 		}
 	}
@@ -52,8 +52,24 @@ void* arena_alloc(Arena *arena, size_t size, size_t align){
 	if(align == 0){
 		align = 1;
 	}
-	if(!find_space(arena, size, align)){
-		arena_grow(arena);
+	if(arena->start && size > arena->region_size){
+		// TODO(...): Unsure if this should be a thing. Right now, the first allocation
+		// defines the scope of an arena, ensuring the congruency of the sizes of regions.
+		// But I don't know if this should be responsibility of the allocator...  
+		// In fact this doesn't solve the possibility of fragmentation. There is no solution
+		// but the informed usage. I have heard that arenas aren't the 'one size fits all'
+		// allocator such as malloc, but at the same time, I can not have a constant
+		// max size for regions.
+		// This will at least help me understand how I would like to use arenas...
+		// TLDR: What to do if I have 4KB regions and need 400MB?
+		fprintf(stderr, "This arena does not allow such allocations.\n");
+		fprintf(stderr, "The local max is %luB.\n", arena->region_size);
+		return NULL;
+	}
+	if(find_space(arena, size, align)){
+		memset((u8*)round_align((uintptr_t)arena->current->avail, align), 0, size);
+	}else if(!arena_grow(arena, size)){
+		return NULL;
 	}
 	void *new_ptr = (void*)round_align((uintptr_t)arena->current->avail, align);
 	arena->current->avail = (void*)(size+(uintptr_t)new_ptr);
@@ -62,7 +78,7 @@ void* arena_alloc(Arena *arena, size_t size, size_t align){
 
 void arena_reset(Arena *arena){
 	for(arena->current = arena->start; arena->current; arena->current = arena->current->next){
-		arena->current->avail = (unsigned char*)arena->current + sizeof(*arena->current);
+		arena->current->avail = (u8*)arena->current + sizeof(*arena->current);
 	}
 	arena->current = arena->start;
 }
@@ -72,9 +88,10 @@ void arena_free(Arena *arena){
 	for(; arena->current;){
 		arena->end = arena->current;
 		arena->current = arena->current->next;
-		munmap(arena->end, REGION_SIZE);
+		munmap(arena->end, arena->region_size);
 	}
 	arena->start = NULL;
 	arena->end = NULL;
 	arena->current = NULL;
+	arena->region_size = 0;
 }
--- arena.h
+++ arena.h
@@ -13,19 +13,22 @@
 #include <stdint.h>
 #include <stddef.h>
 
+typedef uint8_t u8;
+
 typedef struct Region Region;
 struct Region{
 	Region *next;
-	unsigned char *avail;
-	unsigned char *limit;
+	u8 *avail;
+	u8 *limit;
 };
 
 typedef struct {
 	Region *start, *end;
 	Region *current;
+	size_t region_size;
 } Arena;
 
-void arena_grow(Arena*);
+void* arena_grow(Arena*, size_t);
 void* arena_alloc(Arena*, size_t, size_t);
 void arena_free(Arena*);
 void arena_reset(Arena*);
