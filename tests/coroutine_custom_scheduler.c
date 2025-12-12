af0f3d024b5de9df03d9417d85b021f1b631672a
Gabriel Sá <gabriel10043@live.com>
2025-12-12 13:43:18 -0300

    update coroutine_custom_scheduler.test

--- tests/coroutine_custom_scheduler.c
+++ tests/coroutine_custom_scheduler.c
@@ -3,18 +3,15 @@
 
 struct _co_scheduler my_scheduler = {0};
 
-void* co_arg(void *arg){
-	int n = (uintptr_t)arg;
-	for(int i = 0; i < n; i++){
-		printf("%d\n", i);
-		yield(NULL);
+void* co_arg(){
+	if(_co_sched == &my_scheduler){
+		printf("OK!\n");
 	}
 	return NULL;
 }
 
 int main(){
-	coroutine_create(co_arg, (void*)10, .sched=&my_scheduler);
-	coroutine_create(co_arg, (void*)5, .sched=&my_scheduler);
+	coroutine_create(co_arg, NULL, .sched=&my_scheduler);
 	coroutine_start(.sched=&my_scheduler);
 	return 0;
 }
