0b63d82682125db907249a6d09bad497ba5ae76d
Gabriel Sá <gabriel10043@live.com>
2026-02-28 13:06:26 -0300

    add checkpoint test

---
+++ tests/arena_checkpoint.c
@@ -0,0 +1,23 @@
+#define SNORKEL_IMPLEMENTATION
+#include "../snorkel_arena.h"
+#include <stdint.h>
+
+Arena g = {0};
+
+int main(){
+	uintptr_t *ptr = arena_alloc(&g, sizeof(*ptr), ALIGNOF(*ptr));
+	*ptr = (uintptr_t)g.current->avail;
+	arena_flag(&g);
+	if(g.checkpoint->addr == (void*)*ptr){
+		printf("OK\n");
+	}
+	(void) arena_alloc(&g, sizeof(*ptr), ALIGNOF(*ptr));
+	(void) arena_alloc(&g, sizeof(*ptr), ALIGNOF(*ptr));
+	(void) arena_alloc(&g, sizeof(*ptr), ALIGNOF(*ptr));
+	(void) arena_alloc(&g, sizeof(*ptr), ALIGNOF(*ptr));
+	arena_restore(&g);
+	if(g.current->avail == (void*)*ptr){
+		printf("OK\n");
+	}
+	return 0;
+}
